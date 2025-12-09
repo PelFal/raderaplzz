e127373d9ce4da6f5ba8ec1a6b4f05dd2804ba1f
Pergl <per.glansa@gmail.com>
2025-12-09 15:53:50 +0100

    testa 1atest

--- src/test/java/systementor/cidemo1/CalculatorTest.java
+++ src/test/java/systementor/cidemo1/CalculatorTest.java
@@ -6,8 +6,24 @@ import org.junit.jupiter.api.Test;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class CalculatorTest {
+
+    @Mock
+    CountryApiClient apiClient;
+    @InjectMocks
+    CountryService countryService;
     private static final Logger logger = Logger.getLogger(Calculator.class.getName());
     private Calculator calculator;
     private final int a = 1;
@@ -17,7 +33,22 @@ class CalculatorTest {
     void setUp() {
         calculator = new Calculator();
     }
-
+    private static final Country LATVIA = new Country(
+            "Latvia",
+            "Republic of Latvia",
+            "Latvija",
+            "Latvijas Republikas",
+            "Riga",
+            "Europe",
+            "Northern Europe",
+            Map.of(
+                    "lav", "Latvian",
+                    "eng", "English"
+            ),
+            List.of("EST", "LTU", "RUS", "BLR"),
+            1829000,
+            64559.0,
+            "https://goo.gl/maps/iQpUkH7ghq31ZtXe9"
     @Test
     void addShouldReturn3() {
         int result = calculator.add(a,b);
--- src/test/java/systementor/cidemo1/CountryServiceTest.java
+++ src/test/java/systementor/cidemo1/CountryServiceTest.java
@@ -1,17 +1,63 @@
 package systementor.cidemo1;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.when;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
 
 
+@ExtendWith(MockitoExtension.class)
 public class CountryServiceTest {
+    @Mock
+    CountryApiClient ApiClient;
+
+    @InjectMocks
+    CountryService countryService;
+
+    private static final Country LATVIA = new Country(
+            "Latvia",
+            "Republic of Latvia",
+            "Latvija",
+            "Latvijas Republikas",
+            "Riga",
+            "Europe",
+            "Northern Europe",
+            Map.of(
+                    "lav", "Latvian",
+                    "eng", "English"
+            ),
+            List.of("EST", "LTU", "RUS", "BLR"),
+            1829000,
+            64559.0,
+            "https://goo.gl/maps/iQpUkH7ghq31ZtXe9"
+    );
 
     @Test
     void getSortedLaungagesReturnsAlphabeticallySortedLangues() {
-        when(countryApiClient.fetchCountryByName("Latvia"))
+        CountryApiClient CountryApiClient;
+        when(ApiClient.fetchCountryByName("Latvia"))
             .thenReturn(LATVIA);
 
         var result = countryService.getSortedBorders("Latvia");
 
         assertEquals(List.of("English", "Latvian"), result);
     }
+    @Test
+    void getSortedLaungagesReturnsAlphabeticallySortedLangues() {
+        CountryApiClient CountryApiClient;
+        when(ApiClient.fetchCountryByName("Latvia"))
+                .thenReturn(LATVIA);
 
+        var result = countryService.getSortedBorders("Latvia");
+
+        assertEquals(List.of("English", "Latvian"), result);
+    }
 }
